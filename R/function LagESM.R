cf7208703ff28dc9e6c59255378418134d797e75
PeterVerboon <Peter.Verboon@ou.nl>
2019-01-14 11:09:46 +0100

    lag function simplified

--- R/function LagESM.R
+++ R/function LagESM.R
@@ -18,53 +18,21 @@
 
 LagESM <- function(dat, subjnr="subjnr", daynr="daynr", beepnr="beepnr", lagn=1, varnames) {
  
-
 if (lagn > 3) {print("number of lags should not exceed 3"); return() }
    
-## add additional beeps at the end of each day with missings
- 
-  
-vdaynr <- rep(sort(unique(dat[,daynr])), length(unique(dat[,subjnr])));  
-vsubjnr <- rep(unique(dat[,subjnr]), each=length(unique(dat[,daynr])) )
-
-a <- data.frame(cbind(vsubjnr,vdaynr))
-
-a2 <- NULL; a3 <- NULL
-
-                a1 <- a;   a1[,beepnr] <- max(unique(dat[,beepnr])) + 1 
-if (lagn > 1)  {a2 <- a;   a2[,beepnr] <- max(unique(dat[,beepnr])) + 2 }
-if (lagn == 3) {a3 <- a;   a3[,beepnr] <- max(unique(dat[,beepnr])) + 3 }
-                
-               
-a <- rbind(a1,a2,a3)  ;                            
-a[,c(4:dim(dat)[2])] <- NA
-names(a) <- names(dat)
-
-b <- as.data.frame(rbind(dat, a))
-
-# s1 <- b[,subjnr]; s2 <- b[,daynr]; s3 <- b[,beepnr]
-# b <- b[order(s1,s2,s3),]
-
-b <- b[order(b[,subjnr],b[,daynr],b[,beepnr]),]
-
-
-## add lagged variables 
+b <- dat[order(dat[,subjnr],dat[,daynr],dat[,beepnr]),]
 
 L <- length(varnames)
 
-for (i in 1:L) {   
-  newname <- paste(varnames[i],"L",lagn, sep="")
-  b[,newname] <- data.table::shift(b[,c(varnames[i])], n=lagn)
-}
+  for (i in 1:L) {   
+      newname <- paste(varnames[i],"L",lagn, sep="")
+      b <-  DataCombine::slide(b, Var = varnames[i], slideBy = -lagn, NewVar = newname, reminder = FALSE)
+  }
 
-
-## remove additional beeps
-
-b <- b[b[,beepnr] <= (max(unique(dat[,beepnr]))),]
 
 return(b)
 
-}   # end function
+}  
 
 
 
