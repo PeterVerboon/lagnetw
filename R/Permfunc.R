ec9b1b4203e66c4574bb2b9d9eb242d2ab518937
Peter Verboon <peterverboon@iMac2-van-Peter.local>
2019-10-25 11:07:34 +0200

    new functionalities in perDif

--- R/Permfunc.R
+++ R/Permfunc.R
@@ -8,6 +8,7 @@
 #' @param pb name progressbar
 #' @param outnames vector with the names of the variables which are centered.
 #' @param pred formula with names of predictor variables for lmer.
+#' @param subset  subset of predictor variables which are compared in summary statistics. If null then result is also null.                  
 #' @param nobs.per.person vector with number of observations per person             
 #' @param group.per.person vector with group number for each person
 #' @param optim optimizer used in lmer, options: "bobyqa" or "Nelder_Mead", see lmerControl (lme4)           
@@ -17,13 +18,13 @@
 #'         and differences between standard deviations, with their p-values (2 definitions).
 #' @export
 #'
-permfunc <- function(perms, dat, pb, outnames, pred, nobs.per.person, group.per.person, optim = "bobyqa") {
+permfunc <- function(perms, dat, pb, outnames, pred, subset = NULL, nobs.per.person, group.per.person, optim = "bobyqa") {
 
   utils::setTxtProgressBar(pb, perms)
 
    ### set up matrices to store coefficients in
     k <- length(outnames)
-    
+
     b1.perm <- matrix(NA, nrow=k, ncol=k)
     b2.perm <- matrix(NA, nrow=k, ncol=k)
    
@@ -51,7 +52,7 @@ permfunc <- function(perms, dat, pb, outnames, pred, nobs.per.person, group.per.
       if (inherits(res1.perm, "try-error") | inherits(res2.perm, "try-error")) {
          return(NA)
       } else {
-         b1.perm[i,] <- lme4::fixef(res1.perm)[2:(k+1)]
+         b1.perm[i,] <- lme4::fixef(res1.perm)[2:(k+1)]               # the covariates are ignored here
          b2.perm[i,] <- lme4::fixef(res2.perm)[2:(k+1)]
          }
    
@@ -62,12 +63,16 @@ permfunc <- function(perms, dat, pb, outnames, pred, nobs.per.person, group.per.
    b2.permD <- b2.perm
    diag(b1.permD) <- NA
    diag(b2.permD) <- NA
-   sav <- rep(NA_real_, 4)
+   sav <- rep(NA_real_, 5)
    sav[1] <- c(mean(abs(b1.perm)) - mean(abs(b2.perm)))
    sav[2] <- c(mean(abs(diag(b1.perm))) - mean(abs(diag(b2.perm))))
    sav[3] <- mean(abs(b1.permD), na.rm=TRUE) - mean(abs(b2.permD), na.rm=TRUE)
-   sav[4] <- stats::sd(b1.perm, na.rm =TRUE) - stats::sd(b2.perm, na.rm =TRUE)
-   names(sav) <- c("total ", "diagonal ", "off-diag ", "SD ")
+   if (!is.null(subset)) {
+      s <- (outnames %in% subset)*(1:k)
+      sav[4] <- mean(abs(b1.perm[,s]), na.rm=TRUE) - mean(abs(b2.perm[,s]), na.rm=TRUE)
+   } 
+   sav[5] <- stats::sd(b1.perm, na.rm =TRUE) - stats::sd(b2.perm, na.rm =TRUE)
+   names(sav) <- c("total ", "diagonal ", "off-diag ", "subset  ", "SD ")
    
    
    difFE <- b1.perm - b2.perm
--- R/permDif.R
+++ R/permDif.R
@@ -1,12 +1,13 @@
 #' Test the difference of the network connectivity between two groups
 #'
 #' @param dat data frame
-#' @param vars vector with the names of the variables which are centered. 
+#' @param vars vector with the names of the dependent variables. 
+#' @param covs vector with the names of the covariates. 
 #' @param group dichotomous variable that indicates the two groups to be compared
 #' @param subjnr identification number of the subjects
-#' @param level1 variable, indicating level 1 variable, e.g. beeps. Must be specified.
-#' @param level2 variable, indicating level 2 variable, e.g. days. Can be left empty. 
-#' @param randomVars logical, indicating whether the variables should be included as random effects.
+#' @param randomVars vector, indicating which variables should be included as random effects. 
+#'                   If "all" then all fixed effects are taken. If "null" only intercept is used as random effect.
+#' @param subset  subset of predictor variables which are compared in summary statistics. If null then result is also null.                  
 #' @param perms number of permutations.             
 #' @param optim optimizer used in lmer, options: "bobyqa" or "Nelder_Mead", see lmerControl (lme4)           
 #' @import ggplot2
@@ -18,46 +19,53 @@
 #' data("gratitude")
 #' vars <- c("pa_1","pa_2","pa_3","na_1","na_2","na_3")
 #' out <- permDif(dat=gratitude,vars=vars, group="wellBeing", subjnr="subjnr",
-#' level1="beepno", level2 = "dayno", randomVars = FALSE, perms = 10) 
-permDif <- function(dat, vars, group, subjnr, level1, level2 = NULL, randomVars = F, 
+#' randomVars = FALSE, perms = 10) 
+permDif <- function(dat, vars, covs = NULL, group, subjnr, randomVars = NULL, subset = NULL,
                    perms = 500, optim = "bobyqa") {
   
-  res <- list(intermediate = list(),
+  res <- list(input = as.list(environment()),
+              intermediate = list(),
               output = list());
   resall <- list()
   
   dat$group <- dat[,group] 
-   if (is.null(level2)) {
-     level2 <- "ones"
-     dat$ones <- 1
-   }
-  
-  dat1 <- lagnetw::centerESM(data = dat, subjnr = subjnr, addmeans = F, varnames = vars, center = "grand_mean")
-  dat1 <- lagnetw::lagESM(data = dat1, subjnr = subjnr, level1 = level1, level2 = level2, lagn = 1, varnames = vars )
-  
-  k <- length(vars)  
+
+    k <- length(vars)  
+    if (is.null(covs)) p <- k
+    if (!is.null(covs)) p <- k + length(covs)
   
   ### set up matrices to store coefficients in
-  b.diff.obs <- rep(NA_real_, 4)
-  names(b.diff.obs) <- c("total      ", "diagonal    ", "off-diag    ", "standard deviation")
-  b1 <- matrix(NA, nrow=k, ncol=k)
-  b2 <- matrix(NA, nrow=k, ncol=k)
+  b.diff.obs <- rep(NA_real_, 5)
+  names(b.diff.obs) <- c("total      ", "diagonal    ", "off-diag    ", "subset  ", "standard deviation")
+  b1 <- matrix(NA, nrow=k, ncol=p)
+  b2 <- matrix(NA, nrow=k, ncol=p)
   
   ### set up matrix to store the p values in (two definitions ) 
   p1.perm <-  rep(NA, length(b.diff.obs))
-  p2.perm <-  matrix(NA, nrow = k, ncol = k)
+  p2.perm <-  matrix(NA, nrow = k, ncol = p)
   
-  ### create names for lagged vars and random term
-  varsp <- paste0(vars[1],"L",1)
-  for (i in 2:length(vars)) {
-    varsp <- paste0(varsp, " + ", vars[i],"L",1)
+  ### create names for lagged vars and random term 
+  
+  pnames <- varsp <- paste0(vars[1],"L",1)
+    for (i in 2:length(vars)) {
+        pnames <- c(pnames, paste0(vars[i],"L",1))
+        varsp <- paste0(varsp, " + ", vars[i],"L",1)
+    }
+   if (!is.null(covs)) {
+    varsp <- c(varsp, covs)
+    pnames <- c(pnames, covs)
   }
- 
+  
   ## variables as random effect or intercept only
-  if (randomVars == TRUE) {
-    pred1 <- paste0("(", varsp," + (", varsp, "|",subjnr,"))")
-  } else {
-    pred1 <- paste0("(", varsp," + (", 1, "|",subjnr,"))")
+  if (randomVars == "all") {
+    pred1 <- paste0((paste0(varsp, collapse = " + "))," + (",(paste0(varsp, collapse = " + ")), "|",subjnr,")")
+  } 
+  if (is.null(randomVars)) {
+    pred1 <- paste0((paste0(varsp, collapse = " + "))," + (",1, "|",subjnr,")")
+  }
+  if (!is.null(randomVars) & !(randomVars == "all")) {
+    if (sum(!(randomVars %in% names(dat))) > 0) {return("At least one random term is not present in the data")}
+    pred1 <- paste0((paste0(varsp, collapse = " + "))," + (",randomVars, "|",subjnr,")")
   }
  
   ### first choose optimizer
@@ -68,59 +76,77 @@ permDif <- function(dat, vars, group, subjnr, level1, level2 = NULL, randomVars 
     optim <- "bobyqa"
   }
   
+  res$intermediate$formula <- stats::as.formula(paste0(vars[1], "~", pred1, sep=""))
+  
   ## analyses of observed data
   for (i in 1:k) {
     
     ff <- stats::as.formula(paste0(vars[i], "~", pred1, sep=""))
     
     ### fit models
-    res1 <- lme4::lmer(ff, data=subset(dat1, group == 1), REML=FALSE, 
+    res1 <- lme4::lmer(ff, data=subset(dat, group == 1), REML=FALSE, 
                        control = lme4::lmerControl(optimizer = optim, calc.derivs = FALSE))
-    res2 <- lme4::lmer(ff, data=subset(dat1, group == 2), REML=FALSE, 
+    res2 <- lme4::lmer(ff, data=subset(dat, group == 2), REML=FALSE, 
                        control = lme4::lmerControl(optimizer = optim, calc.derivs = FALSE))
     
     ### store coefficients
-    b1[i,] <- lme4::fixef(res1)[2:(k+1)] 
-    b2[i,] <- lme4::fixef(res2)[2:(k+1)]
+    b1[i,] <- lme4::fixef(res1)[2:(p+1)] 
+    b2[i,] <- lme4::fixef(res2)[2:(p+1)]
     
   }
   
-  colnames(p2.perm) <- rownames(p2.perm) <- colnames(b1) <- rownames(b1) <- colnames(b2) <- rownames(b2) <- vars
-  res$output$fixedEffects1 <- b1
-  res$output$fixedEffects2 <- b2
-  res$output$observedDifs <- difs <- b1 - b2
+  rownames(p2.perm) <- rownames(b1) <- rownames(b2) <- vars
+  colnames(p2.perm) <- colnames(b1) <- colnames(b2) <- pnames
+  res$output$fixedEffects1 <- round(b1,4)
+  res$output$fixedEffects2 <- round(b2,4)
+  difs <- b1 - b2
+  res$output$observedDifs <- round(difs, 4) 
+ 
+   
+  ## store differences in observed statistics
+  
+  b.diff.obs[1] <- mean(abs(b1)) - mean(abs(b2))
+  b.diff.obs[2] <- mean(abs(diag(b1))) - mean(abs(diag(b2)))
+  
+  ## next only when a subset of the predictors is requested
+  if (!is.null(subset)) {
+    s <- (vars %in% subset)*(1:k)
+    b.diff.obs[4] <- mean(abs(b1[,s]), na.rm=TRUE) - mean(abs(b2[,s]), na.rm=TRUE)
+    res$intermediate$subset1 <- b1[,s]
+  }
+  
+  ## set diagonal elements to NA (then can take the mean with na.rm=TRUE and get the mean of the off-diagonal elements)
+  diag(b1) <- NA
+  diag(b2) <- NA
+  b.diff.obs[3] <- mean(abs(b1), na.rm=TRUE) - mean(abs(b2), na.rm=TRUE)
+  
+  ## and finally the differences in standard deviations
+  b.diff.obs[5] <- stats::sd(b1, na.rm =TRUE) - stats::sd(b2, na.rm =TRUE)
+  
+  
   
   ### Permutation analyses
   
   ### number of observations per person
-  nobs.per.person  <- sapply(split(dat1$group, dat1$subjnr), length)
+  nobs.per.person  <- sapply(split(dat$group, dat$subjnr), length)
   
   ### group of each person
-  group.per.person <- sapply(split(dat1$group, dat1$subjnr), function(x) x[1])
+  group.per.person <- sapply(split(dat$group, dat$subjnr), function(x) x[1])
   
   ### repeatedly apply permfunc() function
   
   pb1 <- utils::txtProgressBar(min = 0, max = perms, style = 3)
-  permres <- lapply(1:perms, permfunc, dat=dat1, pb = pb1, outnames=vars, 
-                    pred=pred1, nobs.per.person=nobs.per.person, group.per.person=group.per.person)
+  permres <- lapply(1:perms, permfunc, dat=dat, pb = pb1, outnames=vars, 
+                    pred=pred1, subset = subset, nobs.per.person=nobs.per.person, group.per.person=group.per.person)
   close(pb1)
   
   
   ### turn results into an array
   permres <- do.call(Map, c(rbind, permres))
   permres1 <- permres$FEsummary
-  permres2 <- array(permres$FEdifferences,dim = c(6,perms,6))
+  permres2 <- array(permres$FEdifferences,dim = c(k,perms,k))
   
-  ### table with permutation based p-values (two definitions of the p-values)
-  
-  b.diff.obs[1] <- mean(abs(b1)) - mean(abs(b2))
-  b.diff.obs[2] <- mean(abs(diag(b1))) - mean(abs(diag(b2)))
-  
-  ### set diagonal elements to NA (then can take the mean with na.rm=TRUE and get the mean of the off-diagonal elements)
-  diag(b1) <- NA
-  diag(b2) <- NA
-  b.diff.obs[3] <- mean(abs(b1), na.rm=TRUE) - mean(abs(b2), na.rm=TRUE)
-  b.diff.obs[4] <- stats::sd(b1, na.rm =TRUE) - stats::sd(b2, na.rm =TRUE)
+  ### table with permutation based p-values 
   
   for (j in 1:length(b.diff.obs)) {
     p1.perm[j] <- 2*min(mean(permres1[,j] >= b.diff.obs[j], na.rm=TRUE), 
@@ -129,7 +155,7 @@ permDif <- function(dat, vars, group, subjnr, level1, level2 = NULL, randomVars 
   
   
  #  
- #  ### table with model-based and permutation based p-values 
+ #  ### table with permutation based p-values 
    for (i in 1:k) {
      for (j in 1:k) {
      p2.perm[i,j] <- 2*min(mean(permres2[i,,j] >= difs[i,j], na.rm=TRUE),
@@ -141,7 +167,7 @@ permDif <- function(dat, vars, group, subjnr, level1, level2 = NULL, randomVars 
   
   res$output$permutations <- permres1
   res$output$pvalues.summary <- round(cbind("difference" = b.diff.obs, "p_value" = p1.perm), 4)
-  res$output$pvalues.all <- round(p2.perm, 3)
+  res$output$pvalues.all <- round(p2.perm, 3)[,1:k]
   res$output$plimit_adjusted <- noquote(paste0("Bonferroni corrected alpha level: ", round(0.05/(k**2),4)))
   
   
